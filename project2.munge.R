d0d89ac42a679bc53187098ee88ed9df4e99f9fc
beaunorgeot <beaunorgeot@gmail.com>
2016-03-03 18:02:23 -0800

    training is failing

--- project2.munge.R
+++ project2.munge.R
@@ -52,20 +52,36 @@ stud_adjust_responders %>% filter(weight_ipt == ".") %>% summarise(n = n()) #81 
 stud_adjust_responders %>% filter(weight_pwk == ".") %>% summarise(n = n()) #81 people
 
 colnames(stud_adjust_responders)[grep("mtb_",colnames(stud_adjust_responders))] 
-stud_adjust_responders_filtered = stud_adjust_responders %>% select(-c(Obs,age,id,birthregion, cxr_bilat, cd4cells, cd4cells_,mtb_s_on_days_te,mtb_s_all_days_te,mtb_s_on_days_sc,mtb_l_on_days_sc,rptdose,karnofsky))
+stud_adjust_responders_filtered = stud_adjust_responders %>% select(-c(Obs,age,id,birthregion, cxr_bilat, cd4cells, cd4cells_,
+                                                                       mtb_s_on_days_te,mtb_s_all_days_te,mtb_s_on_days_sc,
+                                                                       mtb_l_on_days_sc,rptdose,karnofsky, platelet_))
 
+cont = stud_adjust_responders_filtered %>% select(age_,ast,ast_uln,hgb,platelet,wbc,height,weight_enroll,weight_ipt,weight_pwk,
+                                                  Biolmarker.1,Biomarker2,Biomarker3)
 
+cont$weight_ipt = as.numeric(as.character(cont$weight_ipt))
+cont$weight_pwk = as.numeric(as.character(cont$weight_pwk))
+
+quals = stud_adjust_responders_filtered %>% select(-c(age_,ast,ast_uln,hgb,platelet,wbc,height,weight_enroll,weight_ipt,weight_pwk,
+                                                     Biolmarker.1,Biomarker2,Biomarker3,good_id))
+quals <- as.data.frame(sapply(quals, as.factor))
+stud_adjust_clean = cbind(quals,cont)
+stud_adjust_clean$good_id = stud_adjust_responders_filtered$good_id
+stud_adjust_clean$CAT_response = stud_adjust_responders_filtered$CAT_response
 library(caret)
 set.seed(42)
 #split the training set into a train/test set (which I'm calling validate), so that testSet is a true hold out set
-inTrain <- createDataPartition(stud_adjust_responders_filtered$CAT_response, p = 0.8, list = F)
-train_set <- stud_adjust_responders_filtered[inTrain,] %>% select(-good_id)
-test_set <- stud_adjust_responders_filtered[-inTrain,]
+inTrain <- createDataPartition(stud_adjust_clean$CAT_response, p = 0.8, list = F)
+train_set <- stud_adjust_clean[inTrain,]
+training = train_set %>% select(-c(good_id,CAT_response))
+test_set <- stud_adjust_clean[-inTrain,]
 train_response = train_set$CAT_response
 
-binary_rf_1 = train(train_set,train_response,method = "rf", metric = "Kappa",tuneLength =5,ntree = 50, proximity = T, importance = T, type = 1)
+myControl <- trainControl(method = "repeatedcv", number = 10, repeats = 5 )
+binary_rf_1 = train(training,train_response,method = "rf", metric = "Kappa",tuneLength =5,ntree = 500, 
+                    proximity = T, importance = T, type = 1, trControl = myControl)
 
-testy = test_set %>% select(-c(good_id))
+testy = test_set %>% select(-c(good_id,CAT_response))
 #make predictions
 binary_rf_1_predictions = predict(binary_rf_1,testy)
 binary_rf_1_confus = confusionMatrix(binary_rf_1_predictions,test_set$CAT_response) 
